009df4ccb5a23d0e866f56d67876eb2522b56dc5
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-12-03 14:36:02 +0900

    テーブル作成クエリの作成

---
+++ createTable.sql
@@ -0,0 +1,17 @@
+-- 記事データを格納するテーブル
+create table if not exists articles (
+  article_id integer unsigned auto_increment primary key,
+  title varchar(100) not null,
+  contents text not null,
+  username varchar(100) not null,
+  created_at datetime
+);
+
+-- コメントデータを格納するテーブル
+create table if not exists contents (
+  comment_id integer unsigned auto_increment primary_key,
+  article_id integer unsigned not null,
+  message text not null,
+  created_at datetime,
+  foreign key (article_id) references articles(article_id)
+);
