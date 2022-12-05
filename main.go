80c484be612159844925372b503db50bf26473c6
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-12-05 09:18:56 +0900

    データベースへの接続

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	dbUser := "docker"
+	dbPassword := "docker"
+	dbDatabase := "sampledb"
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPassword, dbDatabase)
+
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		fmt.Println(dbConn)
+		fmt.Println(err)
+	} else {
+		fmt.Println("connect to DB")
+	}
+}
